fd22fa93c660a989fe775836cbd107ab0719c23c
William <williamhjortlund@gmail.com>
2026-06-10 14:19:06 +0200

    Delay i kampen, så alle beskeder ikke er instant

--- combat.cpp
+++ combat.cpp
@@ -1,15 +1,25 @@
 #include "combat.h"
 #include <iostream>
+#include <thread>
+#include <chrono>
+
+void delay(int ms) {
+    std::this_thread::sleep_for(std::chrono::milliseconds(ms));
+}
 
 bool Combat::fight(Monster& playerMonster, Monster& enemyMonster) {
     std::cout << "\n--- KAMP START ---\n";
+    delay(800);
     std::cout << "Dit monster:    ";
     playerMonster.printStats();
+    delay(400);
     std::cout << "Fjende monster: ";
     enemyMonster.printStats();
     std::cout << "\n";
+    delay(1000);
 
-    std::cout << "Dit monster angriber foerst!\n";
+    std::cout << "Dit monster angriber først!\n";
+    delay(1000);
 
     while (playerMonster.isAlive() && enemyMonster.isAlive()) {
         int dmg = playerMonster.getStrength();
@@ -17,6 +27,7 @@ bool Combat::fight(Monster& playerMonster, Monster& enemyMonster) {
         std::cout << playerMonster.getName() << " angriber "
                   << enemyMonster.getName() << " for " << dmg << " skade! "
                   << "(fjende HP: " << enemyMonster.getHp() << ")\n";
+        delay(1000);
 
         if (!enemyMonster.isAlive()) break;
 
@@ -25,9 +36,11 @@ bool Combat::fight(Monster& playerMonster, Monster& enemyMonster) {
         std::cout << enemyMonster.getName() << " angriber "
                   << playerMonster.getName() << " for " << dmg << " skade! "
                   << "(dit HP: " << playerMonster.getHp() << ")\n";
+        delay(1000);
     }
 
     std::cout << "\n";
+    delay(500);
     if (playerMonster.isAlive()) {
         std::cout << "*** " << playerMonster.getName() << " vandt kampen! ***\n";
         return true;
