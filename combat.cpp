54f2726a5ce3da325539646f80c1527279f3a548
William <williamhjortlund@gmail.com>
2026-06-10 15:43:57 +0200

    Det er tilfældigt hvem der starter i en kamp

--- combat.cpp
+++ combat.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <thread>
 #include <chrono>
+#include <random>
 
 void delay(int ms) {
     std::this_thread::sleep_for(std::chrono::milliseconds(ms));
@@ -18,25 +19,54 @@ bool Combat::fight(Monster& playerMonster, Monster& enemyMonster) {
     std::cout << "\n";
     delay(1000);
 
-    std::cout << "Dit monster angriber først!\n";
+    // Tilfældig bestemmelse af hvem der starter
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dis(0, 1);
+    
+    bool playerStarts = dis(gen) == 0;
+    
+    if (playerStarts) {
+        std::cout << "Dit monster angriber først!\n";
+    } else {
+        std::cout << "Fjendens monster angriber først!\n";
+    }
     delay(1000);
 
     while (playerMonster.isAlive() && enemyMonster.isAlive()) {
-        int dmg = playerMonster.getStrength();
-        enemyMonster.takeDamage(dmg);
-        std::cout << playerMonster.getName() << " angriber "
-                  << enemyMonster.getName() << " for " << dmg << " skade! "
-                  << "(fjende HP: " << enemyMonster.getHp() << ")\n";
-        delay(1000);
+        if (playerStarts) {
+            int dmg = playerMonster.getStrength();
+            enemyMonster.takeDamage(dmg);
+            std::cout << playerMonster.getName() << " angriber "
+                      << enemyMonster.getName() << " for " << dmg << " skade! "
+                      << "(fjende HP: " << enemyMonster.getHp() << ")\n";
+            delay(1000);
 
-        if (!enemyMonster.isAlive()) break;
+            if (!enemyMonster.isAlive()) break;
 
-        dmg = enemyMonster.getStrength();
-        playerMonster.takeDamage(dmg);
-        std::cout << enemyMonster.getName() << " angriber "
-                  << playerMonster.getName() << " for " << dmg << " skade! "
-                  << "(dit HP: " << playerMonster.getHp() << ")\n";
-        delay(1000);
+            dmg = enemyMonster.getStrength();
+            playerMonster.takeDamage(dmg);
+            std::cout << enemyMonster.getName() << " angriber "
+                      << playerMonster.getName() << " for " << dmg << " skade! "
+                      << "(dit HP: " << playerMonster.getHp() << ")\n";
+            delay(1000);
+        } else {
+            int dmg = enemyMonster.getStrength();
+            playerMonster.takeDamage(dmg);
+            std::cout << enemyMonster.getName() << " angriber "
+                      << playerMonster.getName() << " for " << dmg << " skade! "
+                      << "(dit HP: " << playerMonster.getHp() << ")\n";
+            delay(1000);
+
+            if (!playerMonster.isAlive()) break;
+
+            dmg = playerMonster.getStrength();
+            enemyMonster.takeDamage(dmg);
+            std::cout << playerMonster.getName() << " angriber "
+                      << enemyMonster.getName() << " for " << dmg << " skade! "
+                      << "(fjende HP: " << enemyMonster.getHp() << ")\n";
+            delay(1000);
+        }
     }
 
     std::cout << "\n";
