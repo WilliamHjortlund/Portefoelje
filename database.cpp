2d2c8f8acd171cb4b7b2abec76c1b45789bc4c3d
William <williamhjortlund@gmail.com>
2026-06-14 15:35:35 +0200

    Man kan nu gemme og loade en karakter og monstre med items

--- database.cpp
+++ database.cpp
@@ -1,6 +1,24 @@
 #include "database.h"
 #include <iostream>
 
+static Item createItemFromName(const std::string& name)
+{
+    if (name == "Sværd")
+        return Item("Sværd", ItemType::STRENGTH_BOOST, 5);
+
+    if (name == "Energidrik")
+        return Item("Energidrik", ItemType::HEALTH_BOOST, 10);
+
+    if (name == "Skjold")
+        return Item("Skjold", ItemType::DEFENSE_BOOST, 3);
+
+    if (name == "Løbesko")
+        return Item("Løbesko", ItemType::STATUS_EFFECT,
+                    StatusEffect::SPEED_II);
+
+    return Item();
+}
+
 Database::Database(const std::string& filename)
 {
     if (sqlite3_open(filename.c_str(), &db) != SQLITE_OK) {
@@ -54,6 +72,12 @@ bool Database::saveCharacter(const Character& character)
         character.getName() + "';";
 
     sqlite3_exec(db, deleteSql.c_str(), nullptr, nullptr, &errMsg);
+
+    std::string deleteItemsSql =
+        "DELETE FROM monster_item WHERE character_name='" +
+        character.getName() + "';";
+    
+    sqlite3_exec(db, deleteItemsSql.c_str(), nullptr, nullptr, &errMsg);
 
     std::string insertCharacter =
         "INSERT OR REPLACE INTO character(name) VALUES('" +
@@ -126,6 +150,46 @@ bool Database::loadCharacter(Character& character)
     }
 
     sqlite3_finalize(stmt);
+
+    // LOAD ITEMS
+    std::string itemSql =
+        "SELECT monster_name, item_name "
+        "FROM monster_item "
+        "WHERE character_name='" +
+        character.getName() + "';";
+
+    sqlite3_stmt* itemStmt = nullptr;
+
+    if (sqlite3_prepare_v2(db, itemSql.c_str(), -1,
+                           &itemStmt, nullptr) == SQLITE_OK)
+    {
+        while (sqlite3_step(itemStmt) == SQLITE_ROW)
+        {
+            std::string monsterName =
+                reinterpret_cast<const char*>(
+                    sqlite3_column_text(itemStmt, 0));
+
+            std::string itemName =
+                reinterpret_cast<const char*>(
+                    sqlite3_column_text(itemStmt, 1));
+
+            Item item = createItemFromName(itemName);
+
+            for (int i = 0; i < character.getMonsterCount(); i++)
+            {
+                Monster* monster = character.getMonster(i);
+
+                if (monster != nullptr &&
+                    monster->getName() == monsterName)
+                {
+                    monster->equipItem(item);
+                    break;
+                }
+            }
+        }
+
+        sqlite3_finalize(itemStmt);
+    }
 
     return foundAny;
 }
